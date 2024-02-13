2c9456aade985811391d937d296d69cf33e961ea
DesiZaharieva <135128496+DesiZaharieva@users.noreply.github.com>
2024-02-13 22:25:17 +0200

    Add files via upload

---
+++ 05_Reveal Words.js
@@ -0,0 +1,17 @@
+function solve(words, template) {
+    'use strict'
+
+    const separatedWords = words.split(', ')
+
+    for( const separatedWord of separatedWords) {
+        template = template.replace('*'.repeat(separatedWord.length), separatedWord)
+    }
+
+
+    console.log(template)
+}
+
+solve('great',
+'softuni is ***** place for learning new programming languages'
+)
+
